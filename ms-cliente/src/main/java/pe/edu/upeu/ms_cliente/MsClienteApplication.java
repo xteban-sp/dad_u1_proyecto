701d8e222d4ac07a948a938160548e128e6adc9d
xteban-sp <xtebanflo@gmail.com>
2026-04-22 07:36:27 -0500

    Creacion de carpetas

---
+++ ms-cliente/src/main/java/pe/edu/upeu/ms_cliente/MsClienteApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.ms_cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsClienteApplication.class, args);
+	}
+
+}
---
+++ ms-ventas/src/main/java/pe/edu/upeu/ms_ventas/MsVentasApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.ms_ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsVentasApplication.class, args);
+	}
+
+}
